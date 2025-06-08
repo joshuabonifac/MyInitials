7743120154391c5014898a9c1b1faa2005d164c3
joshuabonifac <104271125+joshuabonifac@users.noreply.github.com>
2025-06-08 13:39:07 -0400

    Add files via upload

---
+++ MyInitials.cpp
@@ -0,0 +1,14 @@
+// Fun Fact: I was in Darius Da King's older videos, & i had first meet him when i was 7 at a pool
+
+#include <iostream>
+
+// This program print the output of my initials JB in ASCII art style
+int main() {
+	std::cout << "JJJJJ  BBBB\n";
+	std::cout << "  J    B   B\n";
+	std::cout << "  J    B   B\n";
+	std::cout << "  J    BBBB\n";
+	std::cout << "J J    B   B\n";
+	std::cout << "J J    B   B\n";
+	std::cout << " JJ    BBBB\n";
+}
